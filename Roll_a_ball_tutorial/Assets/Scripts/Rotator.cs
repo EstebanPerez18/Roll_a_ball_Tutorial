a8f0d6fb13c9f0815d539ebd0317c2ebf2032957
EstebanPerez18 <s.perezesteban18@gmail.com>
2019-04-04 21:06:30 -0700

    Created Collectable Objects

--- Roll_a_ball_tutorial/Assets/Scripts/Rotator.cs
+++ Roll_a_ball_tutorial/Assets/Scripts/Rotator.cs
@@ -2,17 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Rotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
     }
 }
