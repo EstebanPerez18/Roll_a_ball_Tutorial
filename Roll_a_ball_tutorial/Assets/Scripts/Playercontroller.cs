1f374a5a3adec0509a730b6fd34068f56af9d42c
EstebanPerez18 <s.perezesteban18@gmail.com>
2019-04-04 22:01:38 -0700

    collecting the pickup objects

--- Roll_a_ball_tutorial/Assets/Scripts/Playercontroller.cs
+++ Roll_a_ball_tutorial/Assets/Scripts/Playercontroller.cs
@@ -22,4 +22,12 @@ public class Playercontroller : MonoBehaviour
 
         rb.AddForce(movement * speed);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
 }
